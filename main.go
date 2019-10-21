d36b02f5544b977ed7daa7f5331416d5bb8ada7e
angui001 <1441179353@qq.com>
2019-10-21 14:23:35 +0800

    伊拉克对接

--- main.go
+++ main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	_ "github.com/MobileCPX/FunGame/initial"
-	"github.com/MobileCPX/FunGame/models"
 	_ "github.com/MobileCPX/FunGame/routers"
 	"github.com/astaxie/beego"
 	// "github.com/MobileCPX/FunGame/models"
 )
 
 func main() {
-	models.FileToSql()
+	//models.FileToSql()
 	beego.Run()
 }
